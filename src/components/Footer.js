2d98d153e19c74f4757d3b7eee26e1ae754954ab
nablifedi <fedinabli@ieee.org>
2021-12-21 09:02:33 +0100

    Finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {Form, Row, Col} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import {Row, Col} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 function Footer() {
   const [email, setEmail] = useState('')
@@ -9,65 +9,74 @@ function Footer() {
     <footer>
       <Row className='top'>
         <Col md={2}>
-          <h2>Subscribe to our newsletter</h2>
+          <h5>Subscribe to our newsletter</h5>
           <Row>
-            <Form.Control
+            <input
               type='email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder='Email'
-            ></Form.Control>
-            <img />
+            />
+            <img src='/images/submit.png' />
           </Row>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </p>
         </Col>
         <Col md={2}>
-          <h2>Information</h2>
+          <h4>Information</h4>
           <Link to='/about-us'>About Us</Link> <br />
           <Link to='/information'>Information</Link> <br />
           <Link to='/privacy-policy'>Privacy Policy</Link> <br />
           <Link to='/terms-conditions'>Terms &amp; Conditions</Link> <br />
         </Col>
         <Col md={2}>
-          <h2>Services</h2>
+          <h4>Services</h4>
           <Link to='/about-us'>About Us</Link> <br />
           <Link to='/information'>Information</Link> <br />
           <Link to='/privacy-policy'>Privacy Policy</Link> <br />
           <Link to='/terms-conditions'>Terms &amp; Conditions</Link> <br />
         </Col>
         <Col md={2}>
-          <h2>My Account</h2>
+          <h4>My Account</h4>
           <Link to='/about-us'>About Us</Link> <br />
           <Link to='/information'>Information</Link> <br />
           <Link to='/privacy-policy'>Privacy Policy</Link> <br />
           <Link to='/terms-conditions'>Terms &amp; Conditions</Link> <br />
         </Col>
         <Col md={2}>
-          <h2>Our Offers</h2>
+          <h4>Our Offers</h4>
           <Link to='/about-us'>About Us</Link> <br />
           <Link to='/information'>Information</Link> <br />
           <Link to='/privacy-policy'>Privacy Policy</Link> <br />
           <Link to='/terms-conditions'>Terms &amp; Conditions</Link> <br />
         </Col>
-        <Col md={2}>
-          <img /> <p>+216 123456789</p>
-          <img /> <p className='email'>Email@email.com</p>
-          <img /> <p>00 Rue exemple</p>
+        <Col md={2} className='info'>
+          <Row>
+            <img src='/images/call.png' style={{width: '10%', height: '10%', marginRight: '.5rem'}} /> <h5>+216 123456789</h5>
+          </Row>
+          <Row>
+            <img src='/images/email.png' style={{width: '10%', height: '10%', marginRight: '.5rem'}} /> <h5 className='email'>Email@email.com</h5>
+          </Row>
+          <Row>
+            <img src='/images/location.png' style={{width: '10%', height: '10%', marginRight: '.5rem'}} /> <h5>00 Rue exemple</h5>
+          </Row>
         </Col>
       </Row>
       <hr />
-      <div className='bottom'>
-        <Row md={3}>
-          <img />
-          <img />
-          <img />
-        </Row>
-        <Row md={6}>
+      <Row className='bottom'>
+        <div className='icons'>
+          <img src='/images/facebook.png' />
+          <img src='/images/twitter.png' />
+          <img src='/images/instagram.png' />
+        </div>
+        <div>
           Ecommerce website 2021, all rights reserved
-        </Row>
-        <Row md={3}>
+        </div>
+        <div>
           <img src='/images/brands.png' alt='cards' />
-        </Row>
-      </div>
+        </div>
+      </Row>
     </footer>
   )
 }
